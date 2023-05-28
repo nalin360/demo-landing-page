4ef3e11bff0e7e6007a877f4bf2eb824b05a4a22
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-05-28 21:38:38 +0530

    first section

---
+++ src/page/Mainsection.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header'
+import Sectionone from '../components/sectionone'
+function Mainsection() {
+  return (
+    <div>
+        <Header/>
+        <Sectionone/>
+    </div>
+  )
+}
+
+export default Mainsection
