ca0e691860bdb3489e97291db9e08fee919777ed
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-05-29 09:55:42 +0530

    thirdpage layout commpleted

---
+++ src/page/Thirdpage.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Thirdpage() {
+  return (
+    <div className='third-page'>
+        {/* titile div */}
+        <div className='thirdpage-titles'>
+
+        </div>
+        {/* plans div */}
+        <div className='third-page-plain '>
+            <div>
+                {/* logo and content */}
+                <div className='third-'>
+                    <img src=''/>
+                    <div>
+                        <img/><p></p>
+                    </div>
+                </div>
+                {/* free select */}
+                <div>
+                    <h1></h1>
+                    <button></button>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Thirdpage
