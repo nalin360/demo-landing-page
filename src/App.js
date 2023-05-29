cc58400ec1c3f25da1b6fe4b03932ba30d20b937
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-05-29 10:03:17 +0530

    added secondpage components in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
 import Mainsection from "./page/Mainsection";
+import Secondpage from "./page/Secondpage";
 function App() {
   return (
     <div>
       <Mainsection/>
+      <Secondpage/>
     </div>
   );
 }
