47bbc95d12c5db72bb7d6cc85b900ae80880ab0c
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-05-29 09:57:54 +0530

    thierd page bynamic component yet to complete

---
+++ src/shared/Planbox.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Planbox() {
+  return (
+    <div>
+        {/* plans div */}
+        <div>
+            <div>
+                {/* logo and content */}
+                <div>
+
+                </div>
+                {/* free select */}
+                <div>
+                    <h1></h1>
+                    <button></button>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Planbox
